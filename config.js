09790a1d17d2a06810cd0677bfed22ab3fb73e6a
KosukeKondo0527 <k.kondo@skewers.me>
2025-04-20 05:08:54 +0300

    add configuration and secret handling for production and development environments

---
+++ config.js
@@ -0,0 +1,5 @@
+// 開発環境用の設定ファイル（gitignoreに追加）
+const CONFIG = {
+  MAPBOX_API_KEY: 'pk.eyJ1Ijoia28ya2UiLCJhIjoiY205b2trcG5mMGJxczJxczRnZG4yYzRtcSJ9.va8ZaWJjK7qOjOjHUQCDcg', // MapboxのAPIキーを設定
+  DATA_API_URL:'https://script.google.com/macros/s/AKfycbzRIG3PFPi1j9vppasb2NH8_8uY3aQmLRJZnvQGfqtOMx-SH6_luedThmzdGLbPXMya/exec'
+};
